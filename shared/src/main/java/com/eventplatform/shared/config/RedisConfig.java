db42cafb6ebf226111448af1e7169daa6ae2555a
LalitAditya-t41 <lalitaditya.madhavaneni@think41.com>
2026-03-05 13:50:32 +0530

    Fixed the time issue of the Jackson Mapper

--- shared/src/main/java/com/eventplatform/shared/config/RedisConfig.java
+++ shared/src/main/java/com/eventplatform/shared/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.eventplatform.shared.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ public class RedisConfig {
   public ObjectMapper redisObjectMapper() {
     ObjectMapper mapper = new ObjectMapper();
     mapper.registerModule(new JavaTimeModule());
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     return mapper;
   }
 
